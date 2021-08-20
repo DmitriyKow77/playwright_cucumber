11d8a630672a5715402f087c21b58cf989b2afa1
Dmitriy <dmytro.kovalenkodk@gmail.com>
2021-08-20 12:49:08 +0300

    refactor

--- src/test/java/de/taulinger/bddplay/drivers/BasePage.java
+++ src/test/java/de/taulinger/bddplay/drivers/BasePage.java
@@ -1,7 +1,5 @@
 package de.taulinger.bddplay.drivers;
 
-import com.microsoft.playwright.Page;
-
 public class BasePage {
     public String nameSelector(String name){
         return String.format("css=[name=\"%s\"]", name);
